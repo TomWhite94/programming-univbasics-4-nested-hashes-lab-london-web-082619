cf2313e6f07e779887163abbf97e81d9efd27358
Tom White <tomjameswhite94@gmail.com>
2019-08-21 17:35:01 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -87,7 +87,7 @@ def adding_matz
         }
      }
      programmer_hash2 = {:yukhiri_matsumot => {:known_for => "Ruby", :languages => ["LISP", "C"]}
-programmer_hash.merge(programmer_hash2)
+programmer_hash.merge!(programmer_hash2)
 return programmer_hash
     
 end
